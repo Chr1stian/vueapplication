ac00385a076fa5549e0d958632d194f9d7dc1361
Christian Nyvoll <c.nyvoll@sportradar.com>
2018-08-16 14:57:42 +0200

    Added vuetify and setup card components

---
+++ src/components/Projects.vue
@@ -0,0 +1,50 @@
+<template>
+<v-layout>
+   <v-flex xs12 sm6 offset-sm3>
+     <v-card>
+       <v-card-title primary-title>
+         <div>
+           <h3 class="headline mb-0">Project 1</h3>
+         </div>
+       </v-card-title>
+
+       <v-card-actions>
+         <v-btn flat color="orange">Share</v-btn>
+         <v-btn flat color="orange">Explore</v-btn>
+       </v-card-actions>
+     </v-card>
+     <v-card>
+       <v-card-title primary-title>
+         <div>
+           <h3 class="headline mb-0">Project 2</h3>
+         </div>
+       </v-card-title>
+
+       <v-card-actions>
+         <v-btn flat color="orange">Share</v-btn>
+         <v-btn flat color="orange">Explore</v-btn>
+       </v-card-actions>
+     </v-card>
+   </v-flex>
+ </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'Projects',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+.project-page-div {
+  display: flex;
+  flex: center;
+}
+</style>
